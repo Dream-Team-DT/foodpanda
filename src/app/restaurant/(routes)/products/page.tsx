b2ee6d8fd717455d98fbaa70de84fe9df4b4bf6e
Al amin <alaminmridha2004@gmail.com>
2025-09-12 19:25:03 +0600

    Product card UI/ UX update on my restaurant

--- src/app/restaurant/(routes)/products/page.tsx
+++ src/app/restaurant/(routes)/products/page.tsx
@@ -1,10 +1,24 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
 import { IProduct } from "@/types";
+import { EllipsisVertical, Pencil, Trash2 } from "lucide-react";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Switch } from "@/components/ui/switch";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { DialogTitle } from "@radix-ui/react-dialog";
 
 const Products = () => {
   const [products, setProducts] = useState<IProduct[]>([]);
@@ -24,24 +38,88 @@ const Products = () => {
     fetchProducts();
   }, [session?.user?.restaurantId]);
 
+  console.log(products);
+
   return (
     <div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {products.map(({ _id, name, description, image, price }) => (
-          <Card key={_id} className="rounded-2xl gap-0 overflow-hidden p-0">
-            <div
-              style={{ backgroundImage: `url(${image})` }}
-              className="h-50 bg-cover bg-center bg-amber-300"
-            />
-            <CardContent className="p-4 border-t">
-              <div>
-                <p className="font-medium">{name}</p>
-                <p className="text-xs text-muted-foreground">{description}</p>
-                <p className="mt-2 font-semibold">৳ {price}</p>
-              </div>
-            </CardContent>
-          </Card>
-        ))}
+        {products.map(
+          ({ _id, name, description, image, price, isAvailable }) => (
+            <Card
+              key={_id}
+              className={`relative rounded-2xl w-[23rem] gap-0 overflow-hidden p-0 ${
+                !isAvailable && "order-last"
+              }`}
+            >
+              <div
+                style={{ backgroundImage: `url(${image})` }}
+                className="h-46 bg-cover bg-center"
+              />
+              <CardContent className="px-4 py-2 border-t">
+                <div>
+                  <p className="font-medium">{name}</p>
+                  <p className="text-xs text-muted-foreground">{description}</p>
+                  <p className="mt-2 font-semibold">৳ {price}</p>
+                </div>
+              </CardContent>
+
+              {/* Overlay */}
+              <div
+                className={`absolute size-full top-0 left-0 bg-black/55 ${
+                  isAvailable ? "hidden" : "block"
+                }`}
+              ></div>
+
+              <Popover>
+                <PopoverTrigger className="absolute text-white top-3 right-3 cursor-pointer">
+                  <EllipsisVertical />
+                </PopoverTrigger>
+                <PopoverContent className="p-2 bg-neutral-800 text-accent border-0">
+                  <ul className="space-y-1">
+                    <li className="p-2 hover:bg-accent/10 rounded border-b border-gray-50/30 cursor-pointer  ">
+                      <Dialog>
+                        <DialogTrigger className="flex justify-between items-center w-full">
+                          <p>Update</p>
+                          <Pencil />
+                        </DialogTrigger>
+                        <DialogContent>
+                          <DialogHeader>
+                            <DialogTitle></DialogTitle>
+                            <DialogDescription></DialogDescription>
+                          </DialogHeader>
+                          <div>
+                            <h2>Edit content here</h2>
+                          </div>
+                        </DialogContent>
+                      </Dialog>
+                    </li>
+                    <li className="p-2 hover:bg-accent/10 rounded border-b border-gray-50/30 cursor-pointer flex justify-between items-center">
+                      <p>is Available</p>
+                      <Switch checked={isAvailable} />
+                    </li>
+                    <li className="p-2 rounded border-b border-gray-50/30 cursor-pointer bg-destructive/25 hover:bg-destructive/30 text-destructive">
+                      <Dialog>
+                        <DialogTrigger className="flex justify-between items-center w-full">
+                          <p>Delete</p>
+                          <Trash2 />
+                        </DialogTrigger>
+                        <DialogContent>
+                          <DialogHeader>
+                            <DialogTitle></DialogTitle>
+                            <DialogDescription></DialogDescription>
+                          </DialogHeader>
+                          <div>
+                            <h2>Delete content here</h2>
+                          </div>
+                        </DialogContent>
+                      </Dialog>
+                    </li>
+                  </ul>
+                </PopoverContent>
+              </Popover>
+            </Card>
+          )
+        )}
       </div>
     </div>
   );
